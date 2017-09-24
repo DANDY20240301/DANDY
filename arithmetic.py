80100730b3ea5f66aec87fb347cb16e7f3647bb2
dandan0502 <674198105@qq.com>
2017-09-24 17:21:20 +0800

    full program

--- arithmetic.py
+++ arithmetic.py
@@ -3,28 +3,26 @@ import random
 from matplotlib.cbook import flatten
 from itertools import chain
 from fractions import Fraction
-import os
+import os,copy
+import math
 
 # 定义运算符优先级
-priority = {'+':1,'-':1,'*':2,'÷':2,'(':3,')':0}
+priority = {'+':1,'-':1,'*':2,'÷':2}
 
+# 生成表达式
 def expression():
 	opts = ['+','-','*','÷','/']
-
-	# 每个式子
 	op = [] # 运算符列表
-	# operand = [] #操作数列表
-	tmp = []
-	# fomula = ""
+	tmp = [] # 表达式列表
 	num_op = random.randint(1,9) # 附加功能：生成不定长运算符个数
 	# 生成算式
 	preoperand = 0
 	preop = ''
 	for i in range(num_op+1):
-		if preop == "÷":
+		if preop == '÷':
 			preoperand = random.randint(1,5) # 防止除数为0
 			preop = opts[random.randint(0, 4)]
-		elif preop == "/":
+		elif preop == '/':
 			preoperand = random.randint(1,5)
 			preop = opts[random.randint(0, 3)] # 防止连续两个/,如3/4/5
 		else:
@@ -32,10 +30,57 @@ def expression():
 			preop = opts[random.randint(0, 4)] 
 		tmp.append(str(preoperand))
 		tmp.append(preop)
-	tmp[-1] = "="
-		
-	return tmp
+	no_bracket = tmp.copy()
 
+	# 加括号
+	try:
+		left1_pos = random.randint(0,len(tmp)-4) # 限定左括号位置
+		right1_pos = random.randint(left1_pos+3,len(tmp)-2) # 限定右括号位置
+		tmp.insert(left1_pos,'(')
+		tmp.insert(right1_pos,')')
+		print("".join(tmp[:-1]))
+	except Exception:
+		no_bracket[-1] = "="
+		return no_bracket
+
+	flag1 = 0 # 标记是否有"/"
+	index_list = [] # 有/的下标列表
+	for i in range(len(tmp)-1):
+		if tmp[i] == '/':
+			flag1 =flag1 + 1
+			# index_list.append(tmp.index(i)) # 只索引第一个/
+			index_list.append(i)
+	
+	# 如果没有/
+	if flag1 == 0:
+		try:
+			eval(("".join(tmp[:-1])).replace('÷','/'))
+			# print("".join(tmp[:-1]))
+			tmp[-1] = "="
+			# print("无/加（）")
+			return tmp
+		except Exception:
+			no_bracket[-1] = "="
+			return no_bracket
+	# 如果有/
+	else:
+		for i in index_list:
+			print(tmp[i-1:i+2],index_list)
+			if tmp[i-1].isdigit() and tmp[i+1].isdigit(): # 用了两次index
+				try:
+					eval(("".join(tmp[:-1])).replace('÷','/')) #判断带括号的式子是否合法
+					# print("有/加（）")
+				except Exception:
+					# print("有/不加（）")
+					no_bracket[-1] = "="
+					return no_bracket
+			else:
+				no_bracket[-1] = "="
+				return no_bracket
+		tmp[-1] = "="	
+		return tmp  # 防止第一个正确 第二个蒙混过关
+	
+# 把中缀表达式转为后缀表达式
 def tosuffix(expr):
 	new_list = []
 	# 把分数单独作为一种操作数
@@ -47,28 +92,35 @@ def tosuffix(expr):
 		else :
 			new_list.append(expr[i])
 			i = i+1
+
 	suffix = [] # 后缀表达式
 	stack = [] # 操作符栈
 	for e in new_list:
+		print(suffix)
+		print(stack)
 		if type(e) != str: 
 			suffix.append(e)
 		elif e.isdigit(): # 操作数
 			suffix.append(int(e))
-		elif e == '(': # 左括号
-			stack.append(e)
 		elif e == ')': # 右括号
-			while stack[-1] != '(':
-				suffix.append(stack.pop())
-			stack.pop()
+			tmp_pop = ''
+			while tmp_pop != '(':
+				tmp_pop = stack.pop()
+				if tmp_pop != '(':
+					suffix.append(tmp_pop)
+		elif len(stack) == 0 or stack[-1] == '(' or e == '(': # 左括号
+			stack.append(e)
 		else: #其他运算符
-			while(len(stack) and priority[stack[-1]] >= priority[e]):
+			while(len(stack) and stack[-1] != '(' and priority[stack[-1]] >= priority[e]): # stack[-1] == '(' stack[-1] != '(' 可变优先级
 				suffix.append(stack.pop())
 			stack.append(e)
 	while len(stack): # 把栈弹干净
 		suffix.append(stack.pop())
 	return suffix
 
+# 后缀表达式求值
 def calculate(suffix):
+	print(suffix)
 	calStack = []
 	for s in suffix:
 		if type(s) == int:
@@ -80,8 +132,10 @@ def calculate(suffix):
 			operand1 = calStack.pop()
 			result = doMath(s,operand1,operand2)
 			calStack.append(result)
+	print(calStack[0])
 	return calStack.pop()
 
+# 基本运算
 def doMath(op,op1,op2):
 	if op == '+':
 		return op1+op2
@@ -92,6 +146,7 @@ def doMath(op,op1,op2):
 	elif op == '÷':
 		return Fraction(op1,op2)
 
+# 主函数
 def main(argv):
 	n = ''
 	try:
@@ -107,6 +162,7 @@ def main(argv):
 		elif opt in ("-n", "--n"):
 			n = arg
 
+	# 计算成绩
 	each_score = float(100/int(n))
 	total_score = 0
 	flag = 0
@@ -120,9 +176,9 @@ def main(argv):
 		ans = input()
 		if ans == str(calculate(suffix)):
 			flag+=1
-			print("回答正确！")
+			print("回答正确！\n")
 		else:
-			print("回答错误！")
+			print("回答错误！,正确答案是{}\n".format(calculate(suffix)))
 	print("共答对{}题".format(flag)+"," "本次得分：{}".format(round(each_score*flag)))
 		
 	
